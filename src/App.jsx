9906325720196728e5dc9f491bf430ab41e73681
jarooda <jaluwibowoaji@gmail.com>
2021-01-23 00:25:21 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-bold text-green-800">Hello LPG</h1>
+    </div>
+  );
+}
+
+export default App;
