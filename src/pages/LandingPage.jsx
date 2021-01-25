266a40778af83ae08653acf107338fb3c11c81d1
jarooda <jaluwibowoaji@gmail.com>
2021-01-25 11:37:05 +0700

    make word dynamically appeared when click mode

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,41 +7,42 @@ export default function LandingPage() {
     const [ name, setName ] = useState("");
 
     function jumpToGame(diff) {
-        history.push('/gameplay', { diff, name })
+      let appear = diff === 'Easy' ? 3 : diff === 'Medium' ? 5 : 7
+      history.push('/gameplay', { diff, name, appear })
     }
 
     function jumpToHowToPlay() {
-        history.push('/howtoplay')
+      history.push('/howtoplay')
     }
 
     function jumpToLeaderBoard() {
-        history.push('/leaderboard')
+      history.push('/leaderboard')
     }
 
     function jumpToPreparationPage() {
-        history.push('/preparation')
+      history.push('/preparation')
     }
     
     return (
-        <div className="flex flex-col items-center border">
-          <input type="text" name="name" className="border" placeholder="Type your name" onChange={(e) => setName(e.target.value)} value={name}/>
-          {
-            name === '' || name.trim() === ''
-            ?
-            ""
-            :
-            <div>
-              <button type="button" onClick={() => jumpToGame('Easy')}>Easy</button>
-              <button type="button" onClick={() => jumpToGame('Medium')}>Medium</button>
-              <button type="button" onClick={() => jumpToGame('Hard')}>Hard</button>
-            </div>
-          }
-          <button type="button" onClick={jumpToHowToPlay}> How to play </button>
-          <button type="button"onClick={jumpToLeaderBoard}> Leaderboard </button>
-          <button type="button" onClick={jumpToPreparationPage}> PreparationPage </button>
+      <div className="flex flex-col items-center border">
+        <input type="text" name="name" className="border" placeholder="Type your name" onChange={(e) => setName(e.target.value)} value={name}/>
+        {
+          name === '' || name.trim() === ''
+          ?
+          ""
+          :
+          <div>
+            <button type="button" onClick={() => jumpToGame('Easy')}>Easy</button>
+            <button type="button" onClick={() => jumpToGame('Medium')}>Medium</button>
+            <button type="button" onClick={() => jumpToGame('Hard')}>Hard</button>
+          </div>
+        }
+        <button type="button" onClick={jumpToHowToPlay}> How to play </button>
+        <button type="button"onClick={jumpToLeaderBoard}> Leaderboard </button>
+        <button type="button" onClick={jumpToPreparationPage}> PreparationPage </button>
 
-          {name}
-        </div>
+        {name}
+      </div>
     )
 }
 
