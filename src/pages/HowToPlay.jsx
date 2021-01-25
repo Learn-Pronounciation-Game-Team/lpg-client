b4fa1627738e8705d2b199cbdc8c458e75ea7be0
normndakbr <normnd.akbr2@gmail.com>
2021-01-25 11:31:57 +0700

    Landing Page Done

--- src/pages/HowToPlay.jsx
+++ src/pages/HowToPlay.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import useWindowDimensions from '../helpers/getCurrentWindow'
+
+//Assets
+import gameLogo from '../assets/gameLogo.png'
+import btnBack from '../assets/buttons/btn-back.png'
 
 function HowToPlay() {
     const history = useHistory()
+    const [username, setUsername] = useState();
+    const { height, width } = useWindowDimensions();
 
-    return(
-        <div className="container">
-            <h1> How To Play </h1>
+    function jumpToMain() {
+        history.push('/')
+    }
+
+    return (
+        <div className="max-h-screen" style={{ backgroundColor: '#F0C38E', width: width, height: height }}>
+            <div className="h-screen flex items-center justify-center">
+                <div className="bg-containerMain bg-cover p-24 rounded-3xl shadow-2xl">
+                    <div className="text-4xl uppercase mb-8 text-white flex items-center justify-center font-press-start2p" >
+                        How To Play
+          </div>
+                    <div className="flex items-center justify-center">
+                        <img className="mx-4 w-5/12 h-auto" src={gameLogo} />
+                    </div>
+                    <div className="text-sm mb-8 text-white flex items-center justify-center font-press-start2p">
+                        a learning prononciation game.
+          </div>
+                    <div className="flex justify-center flex-row">
+                        <img className="mx-4" src={btnBack} onClick={jumpToMain} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/PreparationPage.jsx
+++ src/pages/PreparationPage.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import useWindowDimensions from '../helpers/getCurrentWindow'
 
-export default function HowToPlay() {
+//Assets
+import gameLogo from '../assets/gameLogo.png'
+import btnNext from '../assets/buttons/btn-next.png'
+
+export default function PreparationPage() {
     const history = useHistory()
+    const [username, setUsername] = useState();
+    const { height, width } = useWindowDimensions();
 
-    return(
-        <div className="container">
-            <h1> How To Play </h1>
+    function jumpToGame() {
+        history.push('/gameplay')
+    }
+
+    return (
+        <div className="max-h-screen" style={{ backgroundColor: '#F0C38E', width: width, height: height }}>
+            <div className="h-screen flex items-center justify-center">
+                <div className="bg-containerMain bg-cover p-24 rounded-3xl shadow-2xl">
+                    <img className="mx-4 w-5/12 h-auto" src={gameLogo} />
+                    <div className="text-4xl uppercase mb-8 text-white flex items-center justify-center font-press-start2p" >
+                        Who Are You
+                    </div>
+                    <div className="flex items-center justify-center">
+                    </div>
+                    <div className="flex justify-center flex-row">
+                        <img className="mx-4" src={btnNext} onClick={jumpToGame} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
